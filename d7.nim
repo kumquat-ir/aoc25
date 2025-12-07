4fac79b1cd5a5d35a89926cca308058b76d4b040
kumquat-ir <66188216+kumquat-ir@users.noreply.github.com>
2025-12-07 12:09:48 -0500

    day 7

---
+++ d7.nim
@@ -0,0 +1,33 @@
+import std/[sequtils, strutils]
+
+let lines = "d7.input".lines.toSeq
+var
+  beams: set[uint8]
+  mults = newSeq[int](lines[0].len)
+  splits = 0
+
+for i in 0 ..< lines[0].len:
+  if lines[0][i] == 'S':
+    beams.incl uint8 i
+    inc mults[i]
+
+for line in lines[1..^1]:
+  var
+    nbeams: set[uint8]
+    nmults = newSeq[int](line.len)
+  for beam in beams:
+    case line[beam]
+      of '.':
+        nbeams.incl beam
+        nmults[beam] += mults[beam]
+      of '^':
+        nbeams.incl {beam - 1, beam + 1}
+        nmults[beam - 1] += mults[beam]
+        nmults[beam + 1] += mults[beam]
+        inc splits
+      else: raise newException(ValueError, "unknown symbol $1" % $line[beam])
+  beams = nbeams
+  mults = nmults
+
+echo splits
+echo mults.foldl(a + b)
